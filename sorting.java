d9724e99a7de007de33aac52164967e8e9c5377b
Nayaz1 <abmajidniaz@gmail.com>
2023-02-27 19:54:45 +0430

    cantribution

--- sorting.java
+++ sorting.java
@@ -1,7 +1,12 @@
 public class sorting {
   public static void main(String []args){
-    // the sorting in bubble algorithm 
+    // the sorting in bubble algorithm in this algorithm if 
+    // the first elment is bigger than second element it will 
+    // change the location of them if not it will ignore and 
+    // copmare the next elemnt this code will sort upward
+
     int  arry[]={1,11,3,4,15,6,8,9};
+
     for (int i=0;i<arry.length;i++){
       for (int j=0;j<arry.length-1;j++){
         if(arry[j]>arry[j+1]){
@@ -11,5 +16,32 @@ public class sorting {
         }}}
 
     for (int i=0;i<arry.length;i++){
-      System.out.print(arry[i]+"   ");
-    }}}
+      System.out.print(arry[i]+"   ");}
+
+      // the other algorithm of sorting is slection:
+      // it finds the smalist or largist elment and than finds
+      // the second smalist or largist elemnt until end of array
+      // this code will sort backward
+
+      int []arr1={12,3,45,56,67,4,3,56};
+
+      for (int i=0;i<arr1.length;i++){
+        for (int j=0;i<arr1.length;j++){
+          if (arr1[j]>arr1[j+1]){
+            arr1[j]
+            why this one dont add contributiion
+          }
+        }
+      }
+
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+      }}
